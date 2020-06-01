fe26da4d45a523f28896047e5a6a838a3fcc74c3
Christy Rollinson <crollinson@mortonarb.org>
2020-06-01 15:10:54 -0500

    More example code
    This is very porly documented, but you should now have an example of reading in a dataset (pushed direclty to github in data/RAW[...]), doing a bit of stuff, and then saving a "cleaned" copy, which is ignored from github since it is "derived" from this script.  That means that using the dataset on github, anybody can create the "TEST_RAW_CLEANED.csv" file, so there's no need to push it and take up space.

--- TestScript.R
+++ TestScript.R
@@ -1,2 +1,27 @@
 # Goal: This is a dummy test script to help orient with R.
 # setwd("../REU2020-MODIS_phenology/")
+
+# dir.create("data")
+# dir.create("data_processed")
+# Reading in the test datasets
+test <- read.csv("data/RAW_test_data.csv")
+
+dim(test)
+names(test)
+summary(test)
+
+test$Taxon <- as.factor(test$Taxon)
+test$Vernacular <- as.factor(test$Vernacular)
+test$TaxList <- NA # Create a new blank columm
+test$TaxList <- paste(test$Taxon, test$Obs.List, sep="-") # Create a new column combining taxa and list
+head(test)
+summary(test)
+dim(test)
+
+write.csv(test, "data_processed/TEST_RAW_CLEANED.csv", row.names=F)
+
+# RESPONSE ~ PREDICTOR; DEPENDENT ~ INDEPENDENT; Y ~ X; Y=mx+b
+
+# Latitude ~ Taxon
+plot(BgLatitude ~ Taxon, data=test) 
+plot(BgLatitude ~ BgLongitude, data=test)
