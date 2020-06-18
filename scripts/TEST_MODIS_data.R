7e37dbc603f61912da6793fa801155ffb24014c6
aernat <aernat3260@gmail.com>
2020-06-18 15:05:02 -0500

    progress on new figures

--- scripts/TEST_MODIS_data.R
+++ scripts/TEST_MODIS_data.R
@@ -12,54 +12,57 @@ lat.in= 39.788276
 lon.in= -94.010451
 
 #set the data that we want specifically from MODISTools to prods
-prods= MODISTools::mt_products()
+prods <- MODISTools::mt_products()
 summary(prods)
 prods$product
 prods[prods$product == 'MCD12Q2',]
 
 #showing only the bands of interest within the product 'MCD12Q2
-mtbands = MODISTools::mt_bands('MCD12Q2')
+mtbands <- MODISTools::mt_bands('MCD12Q2')
 dim(mtbands)
 
 #get list of dates for greenup
-dat.d8 = MODISTools::mt_dates('MCD12Q2', lat = lat.in, lon = lon.in)
+dat.d8 <- MODISTools::mt_dates('MCD12Q2', lat = lat.in, lon = lon.in)
 dat.d8 #see the temporal range of available data for this area. Should be from 2001 - 2017
 
 mtbands #choose the variables of interest for the next step, !!!! use only the _01 bands !!!!
 #focusing on the variables in question, Here is it Greenup.Num_Modes_01
-dat.tst = MODISTools::mt_subset(product= 'MCD12Q2', band=c('Greenup.Num_Modes_01'), lat=lat.in, lon=lon.in, start= '2005-01-01', end= '2015-01-01', site_name= site.id)
+dat.tst <- MODISTools::mt_subset(product= 'MCD12Q2', band=c('Greenup.Num_Modes_01'), lat=lat.in, lon=lon.in, start= '2005-01-01', end= '2015-01-01', site_name= site.id)
 head(dat.tst)
 dat.tst
 
-dat.tst$value[dat.tst$value == 32767] = NA #value representative of NA in MODIS, this filters that out
+dat.tst$value[dat.tst$value == 32767] <- NA #value representative of NA in MODIS, this filters that out
 
 #some formatting that will change the band names and alter the display of the dates
-dat.tst$calendar_date = as.Date(dat.tst$calendar_date)
-dat.tst$band = as.factor(dat.tst$band)
-dat.tst$band.name = as.factor(sapply(dat.tst$band, FUN =function(x){stringr::str_split(x, '[.]') [[1]][1]}))
+dat.tst$calendar_date <- as.Date(dat.tst$calendar_date)
+dat.tst$band <- as.factor(dat.tst$band)
+dat.tst$band.name <- as.factor(sapply(dat.tst$band, FUN= function(x){stringr::str_split(x, '[.]') [[1]][1]}))
 
-#convert the days since 2001-01-01 value to a calendar date
-dat.tst$value_date = as.Date('1970-01-01') + dat.tst$value
+#convert the days since 1970 value to a calendar date
+dat.tst$value_date <- as.Date('1970-01-01') + dat.tst$value
 
 summary(dat.tst)
 head(dat.tst)
 
 dat.tst
 
-dat.tst$greenup.year = lubridate::year(dat.tst$value_date)
-dat.tst$greenup.yday = lubridate::yday(dat.tst$value_date)
+dat.tst$greenup.year <- lubridate::year(dat.tst$value_date)
+dat.tst$greenup.yday <- lubridate::yday(dat.tst$value_date)
 
 #translate yday to calendar dates
-days.mrk = data.frame(Label= c('Jan 1', 'Feb 1', 'Mar 1', 'Apr1', 'May 1', 'Jun 1', 'Jul 1'),
+days.mrk <- data.frame(Label= c('Jan 1', 'Feb 1', 'Mar 1', 'Apr1', 'May 1', 'Jun 1', 'Jul 1'),
                       Date= c('2019-01-01', '2019-02-01', '2019-03-01', '2019-04-01', '2019-05-01', '2019-06-01', '2019-07-01'))
 days.mrk$mrk.yday <- lubridate::yday(days.mrk$Date)
 
+#dat.tst$value <- (dat.tst$value/days.mrk$mrk.yday)
 #simple plot that should show the data for this farm greening up between 2005 and 2015.
-ggplot(data=dat.tst, mapping = aes(x= greenup.year, y= greenup.yday, color= band.name)) +
-  geom_point() +
-  geom_line() +
-  scale_x_continuous('Year', breaks= seq(from= '2005', to= '2015', by= 2 )) +
-  scale_y_continuous('Day', breaks = days.mrk$mrk.yday, labels = days.mrk$Label)
+ggplot(data= dat.tst, mapping= aes(x= greenup.year, y= greenup.yday)) + 
+  ggtitle('                                           Ivy Branch Farm Greenup Date From 2005 - 2015') +
+  geom_line(color= 'orange') +
+  geom_point(mapping= aes(size= 1, label= band.name)) + 
+  geom_text(label= dat.tst$greenup.yday, color= 'black', size = 5) +
+  scale_x_continuous('YEAR', breaks= seq(from= '2005', to= '2015', by= 1 )) +
+  scale_y_continuous('15% GREENUP POINT (YDay)', breaks= days.mrk$mrk.yday, labels= days.mrk$Label)
 
 
 # Storing the raw MODIS output 
