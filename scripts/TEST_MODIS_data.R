274dcf3ff86dc4c4fda80b9c6070c6a4b63ec516
aernat <aernat3260@gmail.com>
2020-06-18 21:01:29 -0500

    commit before trying something risky
    using the dev.off() function, seems risky.

--- scripts/TEST_MODIS_data.R
+++ scripts/TEST_MODIS_data.R
@@ -27,7 +27,7 @@ dat.d8 #see the temporal range of available data for this area. Should be from 2
 
 mtbands #choose the variables of interest for the next step, !!!! use only the _01 bands !!!!
 #focusing on the variables in question, Here is it Greenup.Num_Modes_01
-dat.tst <- MODISTools::mt_subset(product= 'MCD12Q2', band=c('Greenup.Num_Modes_01'), lat=lat.in, lon=lon.in, start= '2005-01-01', end= '2015-01-01', site_name= site.id)
+dat.tst <- MODISTools::mt_subset(product= 'MCD12Q2', band=c('Greenup.Num_Modes_01', 'MidGreenup.Num_Modes_01'), lat=lat.in, lon=lon.in, start= '2005-01-01', end= '2015-01-01', site_name= site.id)
 head(dat.tst)
 dat.tst
 
@@ -36,7 +36,7 @@ dat.tst$value[dat.tst$value == 32767] <- NA #value representative of NA in MODIS
 #some formatting that will change the band names and alter the display of the dates
 dat.tst$calendar_date <- as.Date(dat.tst$calendar_date)
 dat.tst$band <- as.factor(dat.tst$band)
-dat.tst$band.name <- as.factor(sapply(dat.tst$band, FUN= function(x){stringr::str_split(x, '[.]') [[1]][1]}))
+dat.tst$BAND <- as.factor(sapply(dat.tst$band, FUN= function(x){stringr::str_split(x, '[.]') [[1]][1]}))
 
 #convert the days since 1970 value to a calendar date
 dat.tst$value_date <- as.Date('1970-01-01') + dat.tst$value
@@ -54,19 +54,17 @@ days.mrk <- data.frame(Label= c('Jan 1', 'Feb 1', 'Mar 1', 'Apr1', 'May 1', 'Jun
                       Date= c('2019-01-01', '2019-02-01', '2019-03-01', '2019-04-01', '2019-05-01', '2019-06-01', '2019-07-01'))
 days.mrk$mrk.yday <- lubridate::yday(days.mrk$Date)
 
-#dat.tst$value <- (dat.tst$value/days.mrk$mrk.yday)
 #simple plot that should show the data for this farm greening up between 2005 and 2015.
-ggplot(data= dat.tst, mapping= aes(x= greenup.year, y= greenup.yday)) + 
-  ggtitle('                                           Ivy Branch Farm Greenup Date From 2005 - 2015') +
-  geom_line(color= 'orange') +
-  geom_point(mapping= aes(size= 1, label= band.name)) + 
+ggplot(data= dat.tst, mapping= aes(x= greenup.year, y= greenup.yday)) +
+  ggtitle('Ivy Branch Greenup to Maturity Trends From 2005 - 2015') +
+  geom_line(mapping= aes(color= BAND)) +
+  geom_point(color= 'orange', size= .5) + 
   geom_text(label= dat.tst$greenup.yday, color= 'black', size = 5) +
   scale_x_continuous('YEAR', breaks= seq(from= '2005', to= '2015', by= 1 )) +
-  scale_y_continuous('15% GREENUP POINT (YDay)', breaks= days.mrk$mrk.yday, labels= days.mrk$Label)
-
+  scale_y_continuous('DATE (YDay)', breaks= days.mrk$mrk.yday, labels= days.mrk$Label)
 
 # Storing the raw MODIS output 
 if(!dir.exists("../data_raw/MODIS")) dir.create("../data_raw/MODIS")
 write.csv(dat.tst, file.path("../data_raw/MODIS/", paste0("TEST_Greenup_", site.id, ".csv")), row.names=F)
-                      
-
+png(file='Test_Plot_', site.id, '.png')
+plot(dat.tst)
