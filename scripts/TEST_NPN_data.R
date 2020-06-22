4e8d1e079433ab9e91d51761a77ed1bb3255fcb0
Christy Rollinson <crollinson@mortonarb.org>
2020-06-22 18:02:00 -0500

    example script for downloading NPN data

---
+++ scripts/TEST_NPN_data.R
@@ -0,0 +1,66 @@
+# Download Phenology data from NPN
+# NOTE: for right now, probably just start with the NPN individual phenometrics which will get us the first observation for each individual being observed
+# If we have trouble with this, we'll probably want to play with the raw data to get some better cleaned data
+
+# -----------------------------------
+# NOTE: rnpn needs to be installed from GITHUB -- not CRAN!
+#  i.e. you can NOT just do library('rnpn') -- you'll have an older version
+# -----------------------------------
+# install.packages("devtools")
+# library('devtools')
+# devtools::install_github("usa-npn/rnpn")
+library(rnpn)
+
+path.npn <- "../data_raw/NPN"
+if(!dir.exists(path.npn)) dir.create(path.npn)
+if(!dir.exists(file.path(path.npn, "npn_raw"))) dir.create(file.path(path.npn, "npn_raw"))
+if(!dir.exists(file.path(path.npn, "summaries"))) dir.create(file.path(path.npn, "summaries"))
+# -----------------------------------
+
+
+# -----------------------------------
+# -----------------------------------
+#npn_stations_with_spp
+# Getting a list of all species
+npn.spp <- npn_species()
+npn.quercus <- npn.spp[npn.spp$genus=="Quercus",]
+
+quercus.phenophase <- npn_get_phenophases_for_taxon(genus_ids=unique(npn.quercus$genus_id))
+
+# To get pre-summarized data, use npn_donwload_individual_phenometrics
+# to get help: ?npn_download_individual_phenometrics 
+# species_ids = what species you're interested in; just work with 1 at a time for now; can get rid of if you want everything from our Oak Collection
+# station_ids=26202 gets just The Morton Arboretum Oak Collection (just for now)
+# years = we can narrow this down, but we probably want everything we can get
+# request_source = something the function needs so NPN knows whose using this
+dat.npn <- npn_download_individual_phenometrics(species_ids=npn.quercus$species_id[npn.quercus$species=="alba"],
+                                                station_ids = 26202, years=2000:2020
+                                                , request_source="The Morton Arboretum")
+dat.npn[dat.npn==-9999] <- NA
+unique(dat.npn$phenophase_description)
+summary(dat.npn)
+
+# Note: this has more phenophases that we actually want right now, but lets just stick with it
+write.csv(dat.npn, file.path(path.npn, "TEST_MortonArb_QUAL.csv"), row.names=F)
+
+# Doing another quick search for the Arb, but getting everything from our oak collection; note that it is MUCH slower
+# We're also only get a couple phenophases that we actually care about for this project
+phenophase.leaves <- unique(dat.npn$phenophase_id[dat.npn$phenophase_description %in% c("Breaking leaf buds", "Leaves", "Falling leaves")])
+phenophase.leaves
+
+oak.leaf <- npn_download_individual_phenometrics(phenophase_ids =c(371, 471, 483), station_ids = 26202, years=2000:2020, request_source="The Morton Arboretum")
+oak.leaf[oak.leaf==-9999] <- NA
+summary(oak.leaf)
+dim(oak.leaf)
+
+
+
+length(unique(oak.leaf$species)) # 12 unique species
+length(unique(oak.leaf$individual_id)) # 31 unique trees
+
+write.csv(oak.leaf, file.path(path.npn, "TEST_MortonArb_OakCollection_PhenoLeaf.csv"), row.names=F)
+
+# -----------------------------------
+
+
+
