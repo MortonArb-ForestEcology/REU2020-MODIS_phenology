8ab04187b21fdd86c9f576d49130e0bed352798e
aernat <aernat3260@gmail.com>
2020-06-23 08:46:58 -0500

    MODIS script updated and cleaned up
    This should be ready to combine with NPN/Daymet

--- scripts/TEST_MODIS_data.R
+++ scripts/TEST_MODIS_data.R
@@ -1,4 +1,3 @@
-# Andrew's attempt at recreating the Test_MODIS code that Christy pushed
 #data from MODIS, MCD12Q2 found at https://lpdaac.usgs.gov/products/mcd12q2v006/
 #----------------
 #install.packages("MODISTools")
@@ -7,9 +6,9 @@ library(MODISTools)
 library(ggplot2)
 
 #coordinates found from Google Earth of Andrew's Ivy Branch Farm
-site.id = 'IvyBranchFarm'
-lat.in= 39.788276
-lon.in= -94.010451
+site.id = 'MortonArb'
+lat.in= 41.813583
+lon.in= -88.052897
 
 #set the data that we want specifically from MODISTools to prods
 prods <- MODISTools::mt_products()
@@ -27,44 +26,46 @@ dat.d8 #see the temporal range of available data for this area. Should be from 2
 
 mtbands #choose the variables of interest for the next step, !!!! use only the _01 bands !!!!
 #focusing on the variables in question, Here is it Greenup.Num_Modes_01
-dat.tst <- MODISTools::mt_subset(product= 'MCD12Q2', band=c('Greenup.Num_Modes_01', 'MidGreenup.Num_Modes_01'), lat=lat.in, lon=lon.in, start= '2005-01-01', end= '2015-01-01', site_name= site.id)
-head(dat.tst)
-dat.tst
+dat.MODIS <- MODISTools::mt_subset(product= 'MCD12Q2', band=c('Greenup.Num_Modes_01', 'MidGreenup.Num_Modes_01'), lat=lat.in, lon=lon.in, start= '1995-01-01', site_name= site.id)
+head(dat.MODIS)
+dat.MODIS
 
-dat.tst$value[dat.tst$value == 32767] <- NA #value representative of NA in MODIS, this filters that out
+dat.MODIS$value[dat.MODIS$value == 32767] <- NA #value representative of NA in MODIS, this filters that out
 
 #some formatting that will change the band names and alter the display of the dates
-dat.tst$calendar_date <- as.Date(dat.tst$calendar_date)
-dat.tst$band <- as.factor(dat.tst$band)
-dat.tst$BAND <- as.factor(sapply(dat.tst$band, FUN= function(x){stringr::str_split(x, '[.]') [[1]][1]}))
+dat.MODIS$calendar_date <- as.Date(dat.MODIS$calendar_date)
+dat.MODIS$band <- as.factor(dat.MODIS$band)
+dat.MODIS$BAND <- as.factor(sapply(dat.MODIS$band, FUN= function(x){stringr::str_split(x, '[.]') [[1]][1]}))
 
 #convert the days since 1970 value to a calendar date
-dat.tst$value_date <- as.Date('1970-01-01') + dat.tst$value
+dat.MODIS$value_date <- as.Date('1970-01-01') + dat.MODIS$value
 
-summary(dat.tst)
-head(dat.tst)
+summary(dat.MODIS)
+head(dat.MODIS)
 
-dat.tst
+dat.MODIS
 
-dat.tst$greenup.year <- lubridate::year(dat.tst$value_date)
-dat.tst$greenup.yday <- lubridate::yday(dat.tst$value_date)
+dat.MODIS$greenup.year <- lubridate::year(dat.MODIS$value_date)
+dat.MODIS$greenup.yday <- lubridate::yday(dat.MODIS$value_date)
 
 #translate yday to calendar dates
 days.mrk <- data.frame(Label= c('Jan 1', 'Feb 1', 'Mar 1', 'Apr1', 'May 1', 'Jun 1', 'Jul 1'),
                       Date= c('2019-01-01', '2019-02-01', '2019-03-01', '2019-04-01', '2019-05-01', '2019-06-01', '2019-07-01'))
 days.mrk$mrk.yday <- lubridate::yday(days.mrk$Date)
 
-#simple plot that should show the data for this farm greening up between 2005 and 2015.
-png(width= 750, filename= paste0('Test_Plot_', site.id, '.png'))
-ggplot(data= dat.tst, mapping= aes(x= greenup.year, y= greenup.yday)) +
-  ggtitle('Ivy Branch Farm Greening Trends From 2005 - 2015') +
+#simple plot that should show the data for this area greening up between 2005 and 2015.
+
+#png(width= 750, filename= paste0('MODIS_Plot_', site.id, '.png')) un-commment and run to save plot
+ggplot(data= dat.MODIS, mapping= aes(x= greenup.year, y= greenup.yday)) +
+  ggtitle('MODIS Greenup and Midgreenup') +
   geom_line(mapping= aes(color= BAND)) +
   geom_point(color= 'orange', size= .5) + 
-  geom_text(label= dat.tst$greenup.yday, color= 'black', size = 5) +
-  scale_x_continuous('YEAR', breaks= seq(from= '2005', to= '2015', by= 1 )) +
+  geom_text(label= dat.MODIS$greenup.yday, color= 'black', size = 5) +
+  scale_x_continuous('YEAR', breaks= seq(from= '1995', to='2020', by= 1 )) +
   scale_y_continuous('DATE (YDay)', breaks= days.mrk$mrk.yday, labels= days.mrk$Label)
-dev.off()
+#dev.off() include as is if saving plot
 
 # Storing the raw MODIS output 
 if(!dir.exists("../data_raw/MODIS")) dir.create("../data_raw/MODIS")
-write.csv(dat.tst, file.path("../data_raw/MODIS/", paste0("TEST_Greenup_", site.id, ".csv")), row.names=F)
+write.csv(dat.MODIS, file.path("../data_raw/MODIS/", paste0("MODIS_Greenup_", site.id, ".csv")), row.names=F)
+
