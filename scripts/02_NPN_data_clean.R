642e04798ab2fa06378ee3a792dbdf5f41eaf1fd
aernat <aernat3260@gmail.com>
2020-07-09 09:03:29 -0500

    formatted for cleaning "leaves" phenophase
    This now includes a set of threshods for the leaves data from NPN

--- scripts/02_NPN_data_clean.R
+++ scripts/02_NPN_data_clean.R
@@ -21,6 +21,7 @@ summary(oak.leaf)
 # What this code says data[ROWS, COLUMNS]
 # if the days since last no is greather than 10 OR (| mean OR) there is no "no" observation before a yes
 oak.leaf[oak.leaf$numdays_since_prior_no>10 | is.na(oak.leaf$numdays_since_prior_no), c("first_yes_doy", "first_yes_julian_date")] <- NA
+
 oak.leaf[oak.leaf$numdays_until_next_no>10 | is.na(oak.leaf$numdays_until_next_no), c("last_yes_doy", "last_yes_julian_date")] <- NA
 summary(oak.leaf)
 
@@ -85,11 +86,62 @@ dat.budburst[dat.budburst$phenophase_id==371 & dat.budburst$individual_id==13286
 # ANDREW'S HOMEWORK: Repeat what we did for budburst with Leaves
 # 1. Criteria for bad values
 # 2. Aggregation (copy above loop, switch phenophase out)
-########## --------------------- ################
-# leaves
-summary(oak.leaf[oak.leaf$phenophase_id==483,])
-########## --------------------- ################
 
+########## --------------------- ################
+# Leaves '483'
+########## --------------------- ################
+hist(oak.leaf[oak.leaf$phenophase_id==483, 'first_yes_doy'])
+hist(oak.leaf[oak.leaf$phenophase_id==483, 'last_yes_doy'])
+summary(oak.leaf[oak.leaf$phenophase_id==483,])
+
+# Getting rid of leaf phenophase observations after Sept 22 (first day of Autumn ~265) because we just want new/live leaves per the usanpn "Leaves" description for Tree/Shrub.
+oak.leaf[oak.leaf$phenophase_id==483 & oak.leaf$first_yes_doy>355 & !is.na(oak.leaf$first_yes_doy), c("first_yes_doy", "first_yes_julian_date")] <- NA
+oak.leaf[oak.leaf$phenophase_id==483 & oak.leaf$last_yes_doy>355 & !is.na(oak.leaf$last_yes_doy), c("last_yes_doy", "last_yes_julian_date")] <- NA
+summary(oak.leaf[oak.leaf$phenophase_id==483,])
+dim(oak.leaf[oak.leaf$phenophase_id==483,])
+#left with 7 observations. this removed almost 94% of the data.
+
+# So now we feel like we have relatively good "leaves" data, but we still have multiple observations per tree; options: earliest, latest, mean
+# We're making the data APPROPRIATE for the hypothesis rather than making it MEET the hypothesis
+
+#checking to ensure that the multiple observations issue stil persists in this phenophase
+oak.leaf[oak.leaf$phenophase_id==483 & oak.leaf$individual_id==132863,]
+
+# Aggregating using a formula; in R, y=mx+b is y ~ m*x + b 
+dat.leaves <- data.frame(individual_id=rep(unique(oak.leaf$individual_id[oak.leaf$phenophase_id==483]), each=length(unique(oak.leaf$first_yes_year[oak.leaf$phenophase_id==483]))),
+                           year=unique(oak.leaf$first_yes_year[oak.leaf$phenophase_id==483]))
+summary(dat.leaves)                           
+
+for(IND in unique(dat.leaves$individual_id)){
+  # adding some individual metadata -- this only needs to be done for each tree; we don't care about which year it is
+  dat.leaves[dat.leaves$individual_id==IND, c("site_id", "latitude", "longitude", "species_id", "genus", "species", "common_name")] <- unique(oak.leaf[oak.leaf$individual_id==IND,c("site_id", "latitude", "longitude", "species_id", "genus", "species", "common_name")])
+  
+  for(YR in unique(dat.leaves$year[dat.leaves$individual_id==IND])){
+    # creating a handy index for what row we're working with
+    row.now <- which(dat.leaves$individual_id==IND & dat.leaves$year==YR)
+    
+    # Just narrowing the data frame down to just the part we want to work with
+    dat.tmp <- oak.leaf[oak.leaf$phenophase_id==483 & oak.leaf$individual_id==IND & oak.leaf$first_yes_year==YR,]
+    
+    if(nrow(dat.tmp)==0) next # skips through if there's no data
+    
+    if(nrow(dat.tmp)==1){
+      dat.leaves[row.now, c("first.mean", "first.min", "first.max")] <- dat.tmp$first_yes_doy
+      dat.leaves[row.now, c("last.mean", "last.min", "last.max")] <- dat.tmp$last_yes_doy
+    } else {
+      dat.leaves[row.now, "first.mean"] <- mean(dat.tmp$first_yes_doy, na.rm=T)
+      dat.leaves[row.now, "first.min" ] <- min(dat.tmp$first_yes_doy, na.rm=T)
+      dat.leaves[row.now, "first.max" ] <- max(dat.tmp$first_yes_doy, na.rm=T)
+      
+      dat.leaves[row.now, "last.mean"] <- mean(dat.tmp$last_yes_doy, na.rm=T)
+      dat.leaves[row.now, "last.min" ] <- min(dat.tmp$last_yes_doy, na.rm=T)
+      dat.leaves[row.now, "last.max" ] <- max(dat.tmp$last_yes_doy, na.rm=T)
+    }
+  }
+}
+summary(dat.leaves)
+dim(dat.leaves); dim(oak.leaf[oak.leaf$phenophase_id==483,])
+dat.leaves[dat.leaves$phenophase_id==483 & dat.leaves$individual_id==132863,]
 
 
 
