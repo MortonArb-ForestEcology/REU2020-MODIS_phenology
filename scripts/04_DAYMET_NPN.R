7e0894de25c53a99ad6ebb14c8952809bd065738
aernat <aernat3260@gmail.com>
2020-07-10 15:30:08 -0500

    unfinished creation of a loop to get NPN clean GDD5.cum
    The loop needs to bypass NA values in the dat.budburst$first.mean column. The NA values are valid, but are hanging up the loop.

--- scripts/04_DAYMET_NPN.R
+++ scripts/04_DAYMET_NPN.R
@@ -5,39 +5,37 @@ site.id <- 'MortonArb'
 df.met <- read.csv(file.path('../data_raw/DAYMET', paste0("DAYMET_Data_", site.id, ".csv")))
 summary(df.met)
 
-#get NPN data
-path.npn <- "../data_raw/NPN/"
-if(!dir.exists(path.npn)) dir.create(path.npn)
-oak.leaf <- read.csv(file.path(path.npn, paste0('Quercus_', site.id, '.csv')))
+#get cleaned NPN data
+path.clean <- "../data_raw/NPN/cleaned"
+if(!dir.exists(path.clean)) dir.create(path.clean)
+dat.budburst <- read.csv(file.path(path.clean, paste0('Quercus_bud_', site.id, '.csv')))
+dat.leaves <- read.csv(file.path(path.clean, paste0('Quercus_leaf_', site.id, '.csv')))
 #----------------------------
 
-head(oak.leaf)
+head(dat.budburst)
+head(dat.leaves)
 
-oak.leaf$phases.yday <- (oak.leaf$first_yes_doy + oak.leaf$last_yes_doy)/2 #get a mean of each phenophase yday
+unique(dat.budburst$individual_id)
+unique(dat.leaves$individual_id)
 
-#fixes the loop by getting rid of the .00 decimal of oak.leaf$phases.yday to match df.met$yday
-oak.leaf$phases.yday <- round(oak.leaf$phases.yday)
+#Need a way to pass over the NA values in dat.budburst$first.mean
+for(i in 1:nrow(dat.budburst)) {
+ 
+  bud.year <- dat.budburst[i, "year"]
+  bud.yday <- dat.budburst[i, "first.mean"]
 
-unique(oak.leaf$individual_id)
-
-for(i in 1:nrow(oak.leaf)){
-  # We need to use med.bud
-  bud.yday <- oak.leaf[i, 'phases.yday']
-  bud.year <- oak.leaf[i, "first_yes_year"]
-  
+  ifelse (i = NA, 1) {
+    1
+  }
+    else 1
   # We need to get certain rows --> we need 2 pieces of info to match
-  #  we need BOTH year and yday to match that for the dat.npn row we're working with
-  oak.leaf[i,"GDD5.cum"] <- df.met[df.met$year==bud.year & df.met$yday==bud.yday, "GDD5.cum"]
+  #  we need BOTH year and yday to match that for the dat.MODIS row we're working with
+  dat.budburst[i,"GDD5.cum"] <- df.met[df.met$year==bud.year & df.met$yday==bud.yday,"GDD5.cum"]
+  
 }
 
-tail(oak.leaf)
-
-#this specifies the phenophase by its id. 371 is 'Breaking Leaf Buds'
-oak.budburst <- oak.leaf[oak.leaf$phenophase_id == '371',]
-summary(oak.budburst)
-
-oak.leaves <- oak.leaf[oak.leaf$phenophase_id == '483',]
-summary(oak.leaves)
+View(dat.budburst)
+tail(dat.budburst)
 
 library(ggplot2)
 
@@ -45,12 +43,12 @@ path.png <- '../figures/'
 if(!dir.exists(path.png)) dir.create(path.png, recursive=T)
 png(filename= file.path(path.png, paste0('Quercus_Plot_', site.id, '_NPN.png')))
 
-ggplot(data = oak.budburst, mapping = aes(x= GDD5.cum, y= phases.yday)) + 
-  facet_wrap(oak.budburst$first_yes_year) +
+ggplot(data = dat.budburst, mapping = aes(x= GDD5.cum, y= first.mean)) + 
+  facet_wrap(dat.budburst$year) +
   geom_point()
 
 dev.off()
 dat.processed <- '../data_processed/'
 if(!dir.exists(dat.processed)) dir.create(dat.processed)
-write.csv(oak.budburst, file.path(dat.processed, paste0('Quercus_', site.id, '_NPN_MET.csv')), row.names=F)
-write.csv(oak.leaves, file.path(dat.processed, paste0('Quercus_Leaves', site.id, '_NPN_MET.csv')), row.names=F)
+write.csv(dat.budburst, file.path(dat.processed, paste0('Quercus_bud', site.id, '_NPN_MET.csv')), row.names=F)
+write.csv(dat.leaves, file.path(dat.processed, paste0('Quercus_leaf', site.id, '_NPN_MET.csv')), row.names=F)
