080db6d125c290ad3f384ea2c0466058a8bf2ab7
aernat <aernat3260@gmail.com>
2020-07-10 18:05:28 -0500

    Fixing of loops and addition of ggplots
    The loop was fixed by a syntax error in the if() statment as well as by rounding the NPN data of interest to match the DAYMET data.

--- scripts/04_DAYMET_NPN.R
+++ scripts/04_DAYMET_NPN.R
@@ -18,35 +18,63 @@ head(dat.leaves)
 unique(dat.budburst$individual_id)
 unique(dat.leaves$individual_id)
 
-budburst.list <- list(y = dat.budburst, n = length(dat.budburst$first.mean))
+#these two yday values are calculated to different decimals. One has to conform to the other.
+dat.budburst$first.mean <- round(dat.budburst$first.mean, digits = 0)
+dat.leaves$first.mean <- round(dat.leaves$first.mean, digits = 0)
 
-dat.budburst$GDD5.cum <- NA
-#Need a way to pass over the NA values in dat.budburst$first.mean
+#-------------------------------
+#Bud Burst Onset GDD5
 for(i in 1:nrow(dat.budburst)){
  
   bud.year <- dat.budburst[i, "year"]
   bud.yday <- dat.budburst[i, "first.mean"]
 
-  if(dat.budburst[dat.budburst$first.mean == is.na,]) next
+  if(is.na(dat.budburst$first.mean[i])) next
   # We need to get certain rows --> we need 2 pieces of info to match
   #  we need BOTH year and yday to match that for the dat.MODIS row we're working with
   dat.budburst[i,"GDD5.cum"] <-df.met[df.met$year==bud.year & df.met$yday==bud.yday,"GDD5.cum"]
 }
 
-summary(dat.budburst)
-
 tail(dat.budburst)
+
+#------------------------------
+#Leaves Onset GDD5
+for(i in 1:nrow(dat.leaves)){
+  
+  bud.year <- dat.leaves[i, "year"]
+  bud.yday <- dat.leaves[i, "first.mean"]
+  
+  if(is.na(dat.leaves$first.mean[i])) next
+  # We need to get certain rows --> we need 2 pieces of info to match
+  #  we need BOTH year and yday to match that for the dat.MODIS row we're working with
+  dat.leaves[i,"GDD5.cum"] <-df.met[df.met$year==bud.year & df.met$yday==bud.yday,"GDD5.cum"]
+}
+
+tail(dat.leaves)
 
 library(ggplot2)
 
 path.png <- '../figures/'
 if(!dir.exists(path.png)) dir.create(path.png, recursive=T)
-png(filename= file.path(path.png, paste0('Quercus_Plot_', site.id, '_NPN.png')))
+
+png(filename= file.path(path.png, paste0('Budburst_Onset_', site.id, '_NPN.png')))
 
 ggplot(data = dat.budburst, mapping = aes(x= GDD5.cum, y= first.mean)) + 
   facet_wrap(dat.budburst$year) +
-  geom_point()
+  geom_point(mapping=aes(color=species)) +
+  ggtitle('Growing Degree Days for 12 Quercus species taken from NPN in 2017-2019 at The Morton Arboretum')+
+  scale_x_continuous('5C Growing Degree Days')+
+  scale_y_continuous('Bud Burst Onset (DOY)')
+dev.off()
 
+png(filename= file.path(path.png, paste0('Leaves_Onset_', site.id, '_NPN.png')))
+
+ggplot(data = dat.leaves, mapping = aes(x= GDD5.cum, y= first.mean)) + 
+  facet_wrap(dat.leaves$year) +
+  geom_point(mapping=aes(color=species)) +
+  ggtitle('Growing Degree Days for 12 Quercus species taken from NPN in 2017-2019 at The Morton Arboretum')+
+  scale_x_continuous('5C Growing Degree Days')+
+  scale_y_continuous('Leaves Onset (DOY)')
 dev.off()
 dat.processed <- '../data_processed/'
 if(!dir.exists(dat.processed)) dir.create(dat.processed)
