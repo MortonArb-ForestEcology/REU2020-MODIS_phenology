cb8564d5aab8fd6d25804abd1e4d1930bfcf018f
aernat <aernat3260@gmail.com>
2020-07-16 12:38:42 -0500

    clean up, loop errer still exists
    cleaning up some of the code to make it easier to diagnose

--- scripts/02_Met_data_transform.R
+++ scripts/02_Met_data_transform.R
@@ -24,15 +24,15 @@ path.DAYMET <- '../data_raw/DAYMET'
 if(!dir.exists(path.DAYMET)) dir.create(path.DAYMET)
 #Writing the csv file of lat and longs because daymetr batch function needs to read a file instead of a dataframe
 write.csv(NPN.pts, file.path(path.DAYMET, paste0("NPN_Coords_Raw_", species.name, ".csv")), row.names=FALSE)
-
+summary(NPN.pts)
 #install.packages("daymetr")
 #Downloading all of the daymet data for each point. Internal =TRUE means it creates a nested list. Set false to actually download a file
 lat.list <- daymetr::download_daymet_batch(file_location = file.path(path.DAYMET, paste0("NPN_Coords_Raw_", species.name, ".csv")),
                                            start = 2000,
                                            end = 2019,
                                            internal = T)
-is.recursive(list.met)
-is.atomic(lat.list)
+
+
 # This should give us a list with one layer per site
 length(lat.list)
 names(lat.list) <- NPN.pts$site_id # Giving the different layers of the list the site names they correspond to
@@ -42,15 +42,16 @@ summary(lat.list[[1]][["data"]])
 summary(lat.list)
 
 #Creating a simplified list of the information we want
-list.met <- list(NPN.pts)
+
+list.met <- list()
 for(i in seq_along(lat.list)){
-  list.met[[i]] <- data.frame(site=NPN.pts$site[i], latitude=NPN.pts$latitude[i], longitude=NPN.pts$longitude[i], lat.list[[i]]['data'])
+  list.met[[i]] <- data.frame(site=NPN.pts$site_id[i], latitude=NPN.pts$latitude[i], longitude=NPN.pts$longitude[i], lat.list[[i]]['data'])
 }
-#names(list.met) <-  modis.pts$site
+names(list.met) <-  NPN.pts$site_id
 summary(list.met)
 summary(list.met[[1]])
 
-rm(lat.list) # Removing lat.list to save memory
+#rm(lat.list) # Removing lat.list to save memory
 
 #-----------------------------------------------------------------------#
 #take the daymet data, calculate GDD5 and make it a flattened object
@@ -59,20 +60,20 @@ rm(lat.list) # Removing lat.list to save memory
 # library(dplyr)
 
 #Making sure we only go through relevant years we are calculating GDD5 for
-
-calc.gdd5 <- function(df.met){
-  df.met$Date <- as.Date(paste(df.met$year, df.met$yday, sep="-"), format="%Y-%j")
-  df.met$TMEAN <- (df.met$tmax..deg.c. + df.met$tmin..deg.c.)/2
-  df.met$GDD5 <- ifelse(df.met$TMEAN>5, df.met$TMEAN-5, 0)
-  df.met$GDD5.cum <- NA
+summary(NPN.pts)
+calc.gdd5 <- function(NPN.pts){
+  NPN.pts$Date <- as.Date(paste(NPN.pts$first_yes_year, NPN.pts$first_yes_doy, sep="-"), format="%Y-%j")
+  NPN.pts$TMEAN <- (NPN.pts$tmax..deg.c. + NPN.pts$tmin..deg.c.)/2
+  NPN.pts$GDD5 <- ifelse(NPN.pts$TMEAN>5, NPN.pts$TMEAN-5, 0)
+  NPN.pts$GDD5.cum <- NA
   
   
-  for(YR in min(df.met$year):max(df.met$year)){
+  for(YR in min(NPN.pts$first_yes_year):max(NPN.pts$first_yes_year)){
     #df.yr is all weather data for a year at a location
-    df.yr <- df.met[df.met$year==YR,]
+    df.yr <- NPN.pts[NPN.pts$first_yes_year==YR,]
     
     # Only calculate GDD5 if we have Jan 1; this is Daymet, so it should be fine
-    if(min(df.yr$yday)==1){
+    if(min(df.yr$first_yes_doy)==1){
       # If we have Jan 1, calculate cumulative growing degree-days
       df.yr$GDD5.cum <- cumsum(df.yr$GDD5)
     } else {
@@ -81,53 +82,26 @@ calc.gdd5 <- function(df.met){
     }
     
     # Note: we could have done this differently,but this should be easier to diagnose
-    df.met[df.met$year==YR, "GDD5.cum"] <- df.yr$GDD5.cum
+    NPN.pts[NPN.pts$first_yes_year==YR, "GDD5.cum"] <- df.yr$GDD5.cum
   } # end year loop
-  
-  return(df.met)
-}
-
-calc.gdd5 <- function(df.met){
-  df.met$Date <- as.Date(paste(df.met$year, df.met$yday, sep="-"), format="%Y-%j")
-  df.met$TMEAN <- (df.met$tmax..deg.c. + df.met$tmin..deg.c.)/2
-  df.met$GDD5 <- ifelse(df.met$TMEAN>5, df.met$TMEAN-5, 0)
-  df.met$GDD5.cum <- NA
-  
-  for(ST in min(list.met[site_id]):max(list.met[site_id])){
-    #df.yr is all weather data for a year at a location
-    met.site <- list.met[list.met$site_id==ST,]
-    
-    # Only calculate GDD5 if we have Jan 1; this is Daymet, so it should be fine
-    if(min(met.site[met.site$first_yes_doy==1])){
-      # If we have Jan 1, calculate cumulative growing degree-days
-      met.site$GDD5.cum <- cumsum(met.site$GDD5)
-    } else {
-      # if we're missing Jan 1: still create the column, but don't fill it
-      met.site$GDD5.cum <- NA 
-    }
-    
-    # Note: we could have done this differently, but this should be easier to diagnose
-    NPN.pts[NPN.pts$site_id==ST, "GDD5.cum"] <- NPN.pts$GDD5.cum
-  } # end site loop
   
   return(NPN.pts)
 }
-
   
 
-# Apply our df.met function to all layers of our list;
+# Apply our NPN.pts function to all layers of our list;
 list.met <- lapply(list.met, calc.gdd5)
 summary(list.met[[1]])
 
 # Unlist the met to save it to a dataframe that will be easier to share
-df.met <- dplyr::bind_rows(list.met)
-head(df.met)
+NPN.pts <- dplyr::bind_rows(list.met)
+head(NPN.pts)
 
 # Quick graph to make sure things look okay
 library(ggplot2)
-ggplot(data=df.met) +
+ggplot(data=NPN.pts) +
   geom_line(aes(x=yday, y=GDD5.cum, group=year))
 
 
-write.csv(df.met, file.path(path.DAYMET, paste0("DAYMET_Data_Raw_", site.id, ".csv")), row.names=FALSE)
+write.csv(NPN.pts, file.path(path.DAYMET, paste0("DAYMET_Data_Raw_", site.id, ".csv")), row.names=FALSE)
 
